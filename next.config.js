bf3c51383f530548f1711944c5375b783e13d553
J0nas7 <jonas.sorensen.93dk@gmail.com>
2024-01-27 12:13:37 +0100

    Last commit before first deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Deployment
+   */
+  basePath: process.env.NODE_ENV === "production" ? "/discord_clone" : undefined,
+  assetPrefix: process.env.NODE_ENV === "production" ? "/discord_clone" : undefined,
+  /**
+   * Development
+   */
   reactStrictMode: true,
   images: {
     domains: ["localhost", "developerjones.dk", "uploadthing.com", "utfs.io", "miro.medium.com"]
